446139d8c9e47757da293e67a2aa70c41b5420f8
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2025-01-30 00:40:07 -0500

    imported react

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Container from "react-bootstrap/Container";
 import  MainView  from "./components/main-view/MainView";
