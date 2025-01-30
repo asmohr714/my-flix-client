1540a0d2878a5297dd11a586f4bc0cd123f72afa
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2025-01-29 23:55:55 -0500

    removed bootstrap.min

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import Container from "react-bootstrap/Container";
-import { store } from "./redux/store";
- import { Provider } from "react-redux";
- import "bootstrap/dist/css/bootstrap.min.css";
-
 import  MainView  from "./components/main-view/MainView";
 
 // Import statement to indicate that you need to bundle `./index.scss`
@@ -13,11 +9,9 @@ import  MainView  from "./components/main-view/MainView";
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
     return (
-    <Provider store={store}>
         <Container>
         <MainView />
         </Container>
-    </Provider>
     );
 };
 
