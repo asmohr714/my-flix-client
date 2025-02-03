230c807a310fd21fd540885ab50cbc271096d376
asmohr714 <152821567+asmohr714@users.noreply.github.com>
2025-02-03 11:55:42 -0500

    updated favorites button

--- src/components/movie-view/MovieView.jsx
+++ src/components/movie-view/MovieView.jsx
@@ -66,52 +66,50 @@ export const MovieView = ({ movies, user, token, setUser }) => {
     return (
       <div>
       {movie.Image && (
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img className="w-50" src={movie.Image} alt={movie.Title} />
-      <div style={{ marginLeft: '20px', fontSize: '1rem' }}>
-      <div style= {{ marginLeft: '30px', marginTop: '20px' }}>
-      <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Title: </span>
-      <p>{movie.Title}</p>
-      </div>
-      <div style= {{ marginLeft: '30px' }}>
-      <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Director: </span>
-      <p>{movie.Director.Name}</p>
-      </div>
-      <div style= {{ marginLeft: '30px' }}>
-      <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Bio: </span>
-      <p>{movie.Director.Bio}</p>
-      </div>
-      <div style= {{ marginLeft: '30px' }}>
-      <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Birth: </span>
-      <p>{movie.Director.Birth}</p>
-      </div>
-      <div style= {{ marginLeft: '30px' }}>
-      <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Genre: </span>
-      <p>{movie.Genre.Name}</p>
-      </div>
-      <div style= {{ marginLeft: '30px' }}>
-      <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Description: </span>
-      <p>{movie.Description}</p>
-      </div>
-      <div>
-      <Button onClick={handleAddFavorite} className="btn btn-primary mt-3">
-      Favorite
-      </Button>
-      </div>
-      <div>
-      <Button onClick={handleDeleteFavorite} className="btn btn-primary mt-3">
-      Unfavorite
-      </Button>
-      </div>
-      <div>
-      <Link to={'/'}>
-      <Button style={{ backgroundColor: '#018fab', marginTop: '5px', color: 'white' }} className="back-button">
-      Back
-      </Button>
-      </Link>
-      </div>
-      </div>
-      </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img className="w-50" src={movie.Image} alt={movie.Title} />
+        <div style={{ marginLeft: '20px', fontSize: '1rem' }}>
+          <div style={{ marginLeft: '30px', marginTop: '20px' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Title: </span>
+          <p>{movie.Title}</p>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Director: </span>
+          <p>{movie.Director.Name}</p>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Bio: </span>
+          <p>{movie.Director.Bio}</p>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Birth: </span>
+          <p>{movie.Director.Birth}</p>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Genre: </span>
+          <p>{movie.Genre.Name}</p>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1rem' }}>Description: </span>
+          <p>{movie.Description}</p>
+          </div>
+          <div style={{ display: 'flex', gap: '10px', marginTop: '20px' }}>
+          <Button onClick={handleAddFavorite} className="btn btn-primary" style={{ flex: 1 }}>
+            Favorite
+          </Button>
+          <Button onClick={handleDeleteFavorite} className="btn btn-danger" style={{ flex: 1 }}>
+            Unfavorite
+          </Button>
+          </div>
+          <div>
+          <Link to={'/'}>
+            <Button style={{ backgroundColor: '#018fab', marginTop: '5px', color: 'white' }} className="back-button">
+            Back
+            </Button>
+          </Link>
+          </div>
+        </div>
+        </div>
       )}
       </div>
     );
